12cb6338f82c87db0c2f28ce3b9ff1587011b029
anjaligoswami <agcodes86400@gmail.com>
2021-07-30 03:52:35 +0530

    done with JS and basic UI,game made working

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-let firstCard=6
-let secondCard=2
-let sum = firstCard +secondCard
+let cards= []
+let sum=0
 let message=""
 let hasBlackjack=false
-let isAlive=true
+let isAlive=false
+let player= {
+    name: "Anjali",
+    coins: 200
+}
 
-function startGame(){
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name+": $"+player.coins
+
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() *13) +1
+
+    if(randomNumber>10){
+        return 10
+    }
+
+    else if(randomNumber===1){
+        return 11
+    }
+    else
+     return randomNumber
+}
+
+function startGame() {
+    isAlive =true
+    let firstCard= getRandomNumber()
+    let secondCard=getRandomNumber()
+    cards= [firstCard,secondCard]
+    sum=cards[0] + cards[1]
+
+    renderGame()
+}
+function renderGame(){
+console.log(cards);
+ let j=0
+ cardsEl.textContent = "cards: "
+for(let i=0; i<cards.length; i++)
+{
+    cardsEl.textContent+= cards[i]+" "
+    j++
+}
+ console.log("j", j)
+
     if(sum <21){
 message="you are still in the game 🙂"
 isALive= true
@@ -20,5 +63,23 @@ message="Awww! you lost the game🥺"
 isAlive=false
 }
 
-console.log(message);
+messageEl.textContent = message
+sumEl.textContent="Sum: "+sum
+
+
+}
+
+function newCard() {
+    if(isAlive==true && hasBlackjack==false){
+            let card=getRandomNumber()
+        console.log("card", card)
+
+            sum+=card
+        //console.log(sum);
+
+            cards.push(card)
+        //  console.log(cards.length);
+
+            renderGame()
+    }       
 }
